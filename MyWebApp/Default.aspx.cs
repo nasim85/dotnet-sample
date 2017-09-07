7a37d057dab9abed7d739a458d7bc98901ac0b0b
Nasim <nasimuddin.85@gmail.com>
2017-09-07 10:04:03 +0530

    adding dot net sample

---
+++ MyWebApp/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyWebApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        public int addnumber(int a, int b)
+        {
+            int c;
+            c = a + b;
+            return c;
+        }
+
+        public int dividenumber(int a, int b)
+        {
+            int c;
+            c = a-b;
+            return c;
+        }
+
+    }
+}
---
+++ MyWebAppTests5/DefaultTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyWebApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyWebApp.Tests
+{
+    [TestClass()]
+    public class DefaultTests
+    {
+        [TestMethod()]
+        public void addnumberTest()
+        {
+            Default df = new Default();
+            int c = df.addnumber(2, 3);
+            Assert.AreEqual(5, c);
+        }
+
+        [TestMethod()]
+        public void dividenumberTest()
+        {
+            Default df = new Default();
+            int c = df.dividenumber(3, 2);
+            Assert.AreEqual(1, c);
+        }
+    }
+}
